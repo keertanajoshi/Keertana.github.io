661ceb696d981bb495360c4b62b50a4aaafd7be1
keertana joshi <kirtanabj@gmail.com>
2022-06-01 12:53:13 +0530

    my web page

---
+++ script.js
@@ -0,0 +1,12 @@
+const header = document.querySelector('.navbar');
+// add class navbarDark on navbar scroll
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
